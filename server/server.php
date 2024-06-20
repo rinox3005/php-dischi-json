3a2b68247b10a17c6495a7bae887882bcef001a6
rinox3005 <peppe.rinox@gmail.com>
2024-06-20 22:09:36 +0200

    [UPD] added php function to generate ids for albums and return disc by id

--- server/server.php
+++ server/server.php
@@ -1,7 +1,28 @@
 <?php
-
-$musicDiscs = file_get_contents(__DIR__ . '/musicDiscs.json');
-
 header('Content-Type: application/json');
+$musicDiscs = file_get_contents(__DIR__ . '/musicDiscs.json');
+$discsToSearch = json_decode($musicDiscs, true);
 
-echo $musicDiscs;
+// echo $musicDiscs;
+
+function searchDiscById($id, $discsToSearch)
+{
+    foreach ($discsToSearch as $disc) {
+        if ($disc['id'] == $id) {
+            return $disc;
+        }
+    }
+    return null;
+}
+
+if (isset($_GET['id'])) {
+    $id = intval($_GET['id']);
+    $disc = searchDiscById($id, $discsToSearch);
+    if ($disc) {
+        echo json_encode($disc);
+    } else {
+        echo json_encode(["error" => 'Disc not found']);
+    }
+} else {
+    echo $musicDiscs;
+}
