f5848d45a29d668676124f4a45175ee61068a959
rinox3005 <peppe.rinox@gmail.com>
2024-06-20 22:10:20 +0200

    [UPD] added modal to show album info on click, fixed api call to accept params - BONUS DONE

--- client/src/js/main.js
+++ client/src/js/main.js
@@ -3,16 +3,30 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      title: "PHP JSON DISCS",
       urlAPI: "http://localhost/php-dischi-json/server/server.php",
       musicDiscs: [],
+      infoResults: {},
+      showInfo: false,
     };
   },
   methods: {
-    getMusicDiscs() {
-      axios.get(this.urlAPI).then((response) => {
-        this.musicDiscs = response.data;
+    // funzione che fa la chiamata axios e si aspetta un parametro id per le info della modale, se il parametro non c'é fa la chiamata classica
+    getMusicDiscs(id) {
+      const params = {};
+      if (id) {
+        params.id = id;
+      }
+      axios.get(this.urlAPI, { params }).then((response) => {
+        if (id) {
+          this.infoResults = response.data;
+          this.showInfo = true;
+        } else {
+          this.musicDiscs = response.data;
+        }
       });
+    },
+    closeInfo() {
+      this.showInfo = false;
     },
   },
   created() {
